e60ce6a0801c01f6a72d89563c200afd107698b1
Taylor <tbrit10@gmail.com>
2014-08-22 10:11:48 -0700

    Added CSS to plugin.
    Added styling to plugin. Keeps styling encapsulated within the plugin.

--- plugin version/pp_webservices_plugin.php
+++ plugin version/pp_webservices_plugin.php
@@ -21,6 +21,7 @@ function pp_setup_view_adoptable_page() {
     } else if(is_page_template('adoptable-dogs-template.php')) {
          echo '<script type="text/javascript" src="/wp-content/plugins/pp-webservices/js/xml2json.min.js"></script>
                 <script type="text/javascript" src="/wp-content/plugins/pp-webservices/js/pull_animals.js"></script>
+                <link rel="stylesheet" href="/wp-content/plugins/pp-webservices/css/pp-webservices-style.css">
                 <script type="text/javascript">
                     window.onload = pull_animals("dog") 
                 </script>';
@@ -30,27 +31,15 @@ function pp_setup_view_adoptable_page() {
 
 function pp_setup_view_animal_page_header() {
     if(is_page("View Animal"))  {
-        if(!empty($wp_query->query_vars['animalid']) ) {
-            $animalid = $wp_query->query_vars['animalid'];
-            $xml = file_get_contents("http://www.petango.com/webservices/wsadoption.asmx/AdoptableDetails?authkey=pxmj0427a7afmdgc0v6030lfurxt0ypw57dbs0dr4ga2g2j0a4&animalID=" . $animalid);
-            echo '<script type="text/javascript" src="/wp-content/plugins/pp-webservices/js/xml2json.min.js"></script>
+        echo '<script type="text/javascript" src="/wp-content/plugins/pp-webservices/js/xml2json.min.js"></script>
             <script type="text/javascript" src="/wp-content/plugins/pp-webservices/js/view_animal.js"></script>
-            <script type="text/javascript">
+            <link rel="stylesheet" href="/wp-content/plugins/pp-webservices/css/pp-webservices-style.css">';
+        $animalid = get_query_var('animalid');
+        if(!empty($animalid) ) {
+            $xml = file_get_contents("http://www.petango.com/webservices/wsadoption.asmx/AdoptableDetails?authkey=pxmj0427a7afmdgc0v6030lfurxt0ypw57dbs0dr4ga2g2j0a4&animalID=" . $animalid);
+            echo '<script type="text/javascript">
             var detail =' . json_encode($xml) . ';</script>';
         }
-        //GET DOES NOT WORK WITH WORDPRESS REDIRECTS. CURRENTLY TROUBLESHOOTING ADD_REWRITE_TAGS/ADD_REWRITE_RULES
-        /*if(!isset($_GET)) {
-            echo "Get not set";
-        } else {
-            if(isset($_GET['id']) && is_numeric($_GET['id'])) {
-                $id = $_GET['id'];
-                $xml = file_get_contents("http://www.petango.com/webservices/wsadoption.asmx/AdoptableDetails?authkey=pxmj0427a7afmdgc0v6030lfurxt0ypw57dbs0dr4ga2g2j0a4&animalID=" . $id);
-                echo '<script type="text/javascript" src="/wp-content/plugins/pp-webservices/js/xml2json.min.js"></script>
-                    <script type="text/javascript" src="/wp-content/plugins/pp-webservices/js/view_animal.js"></script>
-                    <script type="text/javascript">
-                    var detail =' . json_encode($xml) . ';</script>';
-            }
-        }*/
     }
 }
 
@@ -61,20 +50,23 @@ function pp_setup_view_animal_page_footer() {
     }
 }
 
-function add_rewrite() {
-    add_rewrite_tag('%animalid%','([^/]*)');
-    add_rewrite_rule('^adopt/meet-adoptable-pets/viewanimal/([^/]*)/?', 'index.php?page_id=12926&animalid=$matches[1]', 'top');
+function pp_add_rewrite() {
+    add_rewrite_rule('adopt/meet-adoptable-pets/viewanimal/([0-9]{1,})/?', 'index.php?page_id=12926&animalid=$matches[1]', 'top');
 }
+
+function pp_add_query_vars_filter( $vars ){
+  $vars[] = "animalid";
+  return $vars;
+}
+add_filter( 'query_vars', 'pp_add_query_vars_filter' );
 
 //Hooks for adding neccissary JS files to adoptable pages.
 add_action('wp_head', 'pp_setup_view_adoptable_page');
 add_action('wp_head', 'pp_setup_view_animal_page_header');
 add_action('wp_footer', 'pp_setup_view_animal_page_footer');
 
-add_action('init', 'add_rewrite');
-//Hooks for creating settings menu.
-//add_action('admin_menu', 'pp_setup_settings_menu');
-//add_action('admin_init', 'pp_register_settings');
+//Hook for rewriting View Animal urls.
+add_action('init', 'pp_add_rewrite');
 
 //Hook for registering the widget
 add_action( 'widgets_init', 'pp_register_widget' );
