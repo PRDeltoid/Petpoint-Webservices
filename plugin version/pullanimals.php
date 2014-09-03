480ad7c3e4a1233029a5a389979dd4509207f71f
Taylor <tbrit10@gmail.com>
2014-09-03 11:19:41 -0700

    Removed hardcoding of auth key from pullanimals
    Removed the hardcoding of the auth key from the request URLs in the pullanimals.php file by including wordpress setup files, so I could access the get_options function from inside the file.

--- plugin version/pullanimals.php
+++ plugin version/pullanimals.php
@@ -1,4 +1,6 @@
 <?php
+    define('WP_USE_THEMES', false);
+    require('../../../wp-blog-header.php');
     if(!isset($_GET)) {
         echo "Get not set";
     } else {
@@ -6,11 +8,11 @@
             $type = $_GET['type'];
             if($_GET['type']=="dog") {
                 header('Content-type: text/xml');
-                $xml = file_get_contents('http://www.petango.com/webservices/wsadoption.asmx/AdoptableSearch?authkey=pxmj0427a7afmdgc0v6030lfurxt0ypw57dbs0dr4ga2g2j0a4&speciesID=1&sex=A&ageGroup=ALL&location=Dog%20Runs&site=Adoptions&onHold=no&orderBy=ID&primaryBreed=All&secondaryBreed=All&specialNeeds=A&noDogs=A&noCats=A&noKids=A&stageid=0');
+                $xml = file_get_contents('http://www.petango.com/webservices/wsadoption.asmx/AdoptableSearch?authkey=' . get_option("pp_auth_key") . '&speciesID=1&sex=A&ageGroup=ALL&location=Dog%20Runs&site=Adoptions&onHold=no&orderBy=ID&primaryBreed=All&secondaryBreed=All&specialNeeds=A&noDogs=A&noCats=A&noKids=A&stageid=0');
                 echo $xml;
             } else if($_GET['type']=="cat") {
                 header('Content-type: text/xml');
-                $xml = file_get_contents('http://www.petango.com/webservices/wsadoption.asmx/AdoptableSearch?authkey=pxmj0427a7afmdgc0v6030lfurxt0ypw57dbs0dr4ga2g2j0a4&speciesID=2&sex=A&ageGroup=ALL&location=Cattery&site=Adoptions&onHold=no&orderBy=ID&primaryBreed=All&secondaryBreed=All&specialNeeds=A&noDogs=A&noCats=A&noKids=A&stageid=0');
+                $xml = file_get_contents('http://www.petango.com/webservices/wsadoption.asmx/AdoptableSearch?authkey=' . get_option("pp_auth_key") . '&speciesID=2&sex=A&ageGroup=ALL&location=Cattery&site=Adoptions&onHold=no&orderBy=ID&primaryBreed=All&secondaryBreed=All&specialNeeds=A&noDogs=A&noCats=A&noKids=A&stageid=0');
                 echo $xml;
             } else {
                 echo "Unknown type set";
