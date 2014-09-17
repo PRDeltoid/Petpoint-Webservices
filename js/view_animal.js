8f2bbeaf88c9a2b37bb030ae33063122054611d1
Taylor Britton <tbrit10@gmail.com>
2014-09-17 13:41:32 -0700

    added create_html_node function
    Created a function called create_html_node to reduce the amount of code needed to create an HTML node. The function signature is create_html_node(node_type, attributes (array), parent_node, child_nodes (array), html_content (string))
    
    Node Type takes a standard html node type (div, table, tr, td, img, etc)
    
    Attributes takes an array of objects with the signature {name: attribute name, value: attribute value}
    
    Parent node is where the created node will be attached to.
    
    Child nodes accepts an array of create_html_node calls, and then appends all child nodes created this way into the main node.
    
    Html content sets the innerHTML property of the node to the given value.
    
    Also replaced all manual calls to create and set up nodes with the new function.

--- js/view_animal.js
+++ js/view_animal.js
@@ -3,18 +3,8 @@ function view_animal(animal_id, plugin_base)
     var requestURL = plugin_base + "/viewanimal.php?id=" + animal_id
 
     //request animal data
-    var animal_details;
-    jQuery.ajax({
-        type: 'GET',
-        url: requestURL,
-        dataType: 'json',
-        success: function(data) {
-            animal_details=data;
-        },
-        data: {},
-        async: false
-    });
-
+    var animal_details = get_animal_details(requestURL);
+    
     var output_area = document.getElementById('animal'); //The div element that our HTML will be placed inside of.
 
     //Fields here will show up on the page. The title is what you want the user to see, and the field_name is the name of the variable in the animal_details object. 
@@ -36,32 +26,18 @@ function view_animal(animal_id, plugin_base)
 
     //Create the animal's picture element.
 
-    var animal_picture_container_node = document.createElement("div");
-    animal_picture_container_node.setAttribute("class", "animal-picture-container");
+    var animal_picture_container_node = 
+            create_html_node('div', [ {name:'class',  value: 'animal-picture-container'} ], output_area, 
+                [create_html_node('img', [ {name: 'class', value: 'view-animal-picture'},
+                                          {name: 'id',    value: 'animal-picture'},
+                                          {name: 'src',   value: animal_details['Photo1']}]),
+                create_html_node('div', [ {name: 'id', value: 'photo-links'} ]) ]
+            );
 
-    var animal_picture_node = document.createElement("img");
-    animal_picture_node.setAttribute("class", "view-animal-picture")
-    animal_picture_node.setAttribute("id", "animal-picture")
-    animal_picture_node.setAttribute("src", animal_details["Photo1"])
-    //animal_picture_node.setAttribute("style", "border: 3px solid " + animal_details["BehaviorResult"]); 
-    animal_picture_container_node.appendChild(animal_picture_node);
-
-
-    //Create the buttons to view additional pictures of the animal.
-    var animal_pic_link_node = document.createElement("div");
-    animal_pic_link_node.setAttribute("id", "photo-links");
-    output_area.appendChild(animal_pic_link_node);
     setup_photo_links(animal_details);
-    animal_picture_container_node.appendChild(animal_pic_link_node);
-    
-    //Place the picture container node on the page
-    output_area.appendChild(animal_picture_container_node);
 
-    //Create the animal's detail table.
-    var animal_detail_node = document.createElement("table");
-    animal_detail_node.setAttribute("class", "animal-detail");
-
-    console.log(animal_details);
+    //Create the animal's detail table (this is an empty container at this point).
+    var animal_detail_node = create_html_node('table', [{name:'class', value:'animal-detail'}], output_area); 
 
     //Go through each object in output_fields and output a table row containing the title, and the animal's detail for the field_name
     output_fields.map(function(field_object) {
@@ -71,25 +47,40 @@ function view_animal(animal_id, plugin_base)
                 animal_details[field_object.field_name] = format_field(field_object, animal_details[field_object.field_name]);
             }
 
-            var detail_row_node = document.createElement('tr');
-
-            //Create the title
-            var detail_data_name_node = document.createElement('td');
-            detail_data_name_node.innerHTML = "<b>" + field_object.title + "</b>"
-            detail_row_node.appendChild(detail_data_name_node);
-
-            //Create the animal detail data
-            var detail_data_node = document.createElement('td');
-            detail_data_node.innerHTML = animal_details[field_object.field_name];
-            detail_row_node.appendChild(detail_data_node);
-
-            //Add it all to the animal_detail_node table.
-            animal_detail_node.appendChild(detail_row_node);
+            create_html_node('tr', null, animal_detail_node, [
+                create_html_node('td', null, null, null, "<b>" + field_object.title + "</b>"),
+                create_html_node('td', null, null, null, animal_details[field_object.field_name])  
+            ], null);
         }
     });
+}
 
-    //Add the detail to the page.
-    output_area.appendChild(animal_detail_node);
+function create_html_node(node_type, attributes, parent_node, child_nodes, html_content) {
+
+    var node = document.createElement(node_type);
+    //Set node attributes
+    if(attributes) {
+        attributes.map(function(attribute) {
+            node.setAttribute(attribute.name, attribute.value);
+        });
+    }
+
+    if(html_content) {
+        node.innerHTML = html_content;
+    }
+
+    //Set up any child nodes (recursive)
+    if(child_nodes) {
+        child_nodes.map(function(child_node) {
+            node.appendChild(child_node);
+        });
+    }
+    //Append to the parent_node, if set.
+    if(parent_node) {
+        parent_node.appendChild(node);
+    }
+
+    return node;
 }
 
 function load_photo(photo_url) {
@@ -169,4 +160,19 @@ function format_description(desc) {
 function format_be(be_result) {
     //Colors the result as the behavior result's color. Also adds a small Superscript link at the end to link to a page where the BE colors are explained
     return "<span style='font-weight:bold; color: " + be_result + "'>" + be_result + "</span>   <sup><a href='#' target='_blank'>What is this?</a></sup>";
+}
+
+function get_animal_details(requestURL) {
+    var animal_details;
+    jQuery.ajax({
+            type: 'GET',
+            url: requestURL,
+            dataType: 'json',
+            success: function(data) {
+                animal_details=data;
+            },
+            data: {},
+            async: false
+        });
+    return animal_details;
 }
