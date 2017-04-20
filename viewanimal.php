c271c763a63b2a9c29e022b7692cfa6314fad3f8
Taylor <PRDeltoid@users.noreply.github.com>
2017-04-20 10:30:39 -0700

    Updated PP Webservices URLs
    Changed the subdomain from www to ws, as per PetPoint changes

--- viewanimal.php
+++ viewanimal.php
@@ -6,7 +6,7 @@
     } else {
         if(isset($_GET['id']) && is_numeric($_GET['id'])) {
             header('Content-type: application/json');
-            $xml_string = file_get_contents("http://www.petango.com/webservices/wsadoption.asmx/AdoptableDetails?authkey=" . get_option('pp_auth_key') . "&animalID=" . $_GET['id']);
+            $xml_string = file_get_contents("http://ws.petango.com/webservices/wsadoption.asmx/AdoptableDetails?authkey=" . get_option('pp_auth_key') . "&animalID=" . $_GET['id']);
             $xml = simplexml_load_string($xml_string);
             $json = json_encode($xml);
             echo $json;
